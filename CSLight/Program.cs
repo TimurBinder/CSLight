c45800489de0e64b4ad2b14741d7f0adb98815d2
TimurBindex <nimborlimbo@mail.ru>
2023-12-08 23:00:11 +0500

    Class Menu

--- CSLight/Program.cs
+++ CSLight/Program.cs
@@ -25,6 +25,98 @@
         }
     }
 
+    class Menu
+    {
+        private ConsoleColor _defaultColor;
+        private ConsoleColor _selectedColor;
+        private List<string> _itemsList;
+        private int _selectedIndex;
+        private bool _isOpen;
+
+        public Menu(ConsoleColor defaultColor, ConsoleColor selectedColor, string[] itemsArray)
+        {
+            _defaultColor = defaultColor;
+            _selectedColor = selectedColor;
+            _itemsList = new List<string>(itemsArray);
+            _selectedIndex = 0;
+        }
+
+        public void AddItem(string name)
+        {
+            _itemsList.Add(name);
+        }
+
+        public void RemoveAt(int index)
+        {
+            _itemsList.RemoveAt(index);
+        }
+
+        public int TryGetItemIndex(out int index)
+        {
+            _isOpen = true;
+
+            while (_isOpen)
+            {
+                ShowMenu();
+
+                ConsoleKeyInfo userKey = Console.ReadKey();
+
+                if (KeyInputHandle(userKey) == false)
+                    return false;
+            }
+
+            index = _selectedIndex;
+            return true;
+        }
+
+        private void KeyInputHandle(ConsoleKeyInfo userKey)
+        {
+            switch (userKey.Key)
+            {
+                case ConsoleKey.UpArrow:
+                    if (_selectedIndex > 0)
+                        _selectedIndex--;
+                    else
+                        _selectedIndex = _itemsList.Count - 1;
+
+                    break;
+
+                case ConsoleKey.DownArrow:
+                    if (_selectedIndex < _itemsList.Count - 1)
+                        _selectedIndex++;
+                    else
+                        _selectedIndex = 0;
+
+                    break;
+
+                case ConsoleKey.Enter:
+                    _isOpen = false;
+                    break;
+
+                case ConsoleKey.Escape:
+                    _isOpen = false;
+                    _selectedIndex = -1;
+                    return false;
+
+            }
+
+            return true;
+        }
+
+        private void ShowMenu()
+        {
+            for (var i = 0; i < _itemsList.Count; i++)
+            {
+                if (i == selectedIndex)
+                    Console.ForegroundColor = selectedColor;
+
+                _itemsList[i].ShowInfo();
+
+                Console.ForegroundColor = defaultColor;
+            }
+        }
+    }
+
     class Station
     {
         private Random _random;
@@ -51,6 +143,61 @@
 
             _minDirectionPassengersCount = 300;
             _maxDirectionPassengersCount = 800;
+        }
+
+        public Direction TryGetDirection(out Direction direction)
+        {
+            bool isOpenMenu = true;
+            ConsoleColor defaultColor = Console.ForegroundColor;
+            ConsoleColor selectedColor = ConsoleColor.Green;
+            int selectedIndex = 0;
+
+            while (isOpenMenu)
+            {
+                for (var i = 0; i < _directionsList.Count; i++)
+                {
+                    if (i == selectedIndex)
+                        Console.ForegroundColor = selectedColor;
+
+                    _directionsList[i].ShowInfo();
+
+                    Console.ForegroundColor = defaultColor;
+                }
+
+                ConsoleKeyInfo userKey = Console.ReadKey();
+
+                switch (userKey.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (selectedIndex > 0)
+                            selectedIndex--;
+                        else 
+                            selectedIndex = _directionsList.Count - 1;
+
+                        break;
+
+                    case ConsoleKey.DownArrow:
+                        if (selectedIndex < _directionsList.Count - 1)
+                            selectedIndex++;
+                        else
+                            selectedIndex = 0;
+
+                        break;
+
+                    case ConsoleKey.Enter:
+                        isOpenMenu = false;
+                        break;
+
+                    case ConsoleKey.Escape:
+                        direction = null;
+                        return false;
+                        
+                }
+            }
+
+            direction = _directionsList[selectedIndex];
+            _directionsList.RemoveAt(selectedIndex);
+            return true;
         }
 
         public void AddRandomDirection()
@@ -91,6 +238,11 @@
         public string StartPoint { get; }
         public string FinishPoint { get; }
         public int PassengersCount { get; }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Путь {StartPoint}-{FinishPoint}: всего пассажиров {PassengersCount});
+        }
     }
 
     class Train
